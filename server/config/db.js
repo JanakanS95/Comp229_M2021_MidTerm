c271a4f7bda1ecfc1ea6490eec35a728ba04097b
Janakan Sivananthan <J.Sivananthan1@gmail.com>
2021-06-24 18:24:51 -0400

    Midterm for comp 229

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+
+  //MongoDB Atlas Server
+  "URI": "mongodb+srv://janakan:4ojRk5ECEEK5nUWx@mongodbserver.hfha1.mongodb.net/book_store?retryWrites=true&w=majority"
+};
